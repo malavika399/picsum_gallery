d944a984c3e5f9c828b20a8a426a9953e09dc731
malavika399 <166157548+malavika399@users.noreply.github.com>
2026-06-01 11:12:32 +0530

    Add ios/Runner/GeneratedPluginRegistrant.m

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+}
+
+@end
